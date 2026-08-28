d5e7270e656b37b1972a64fa66cf3e27ca02081c
gulnarfeyzullayeva <gfeyzullayevaa@gmail.com>
2026-08-28 22:21:13 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+// Task-01
+// console.log("My name is Gulnar");
+
+// Task-02
+// const name = "Gulnar";
+// console.log(name);
+
+// Task-03
+// Deyisenin adinda yalniz herf, reqem (adin evveli istisna) ve $ _ simvollarindan istifade etmek olar. CAVAB: person, $add (console de olmaz cunki jsde mueyyen funksiyasi var)
+
+// Task-04
+// let bookPrice = 20;
+// let bookAmount = 100;
+// console.log(bookPrice*bookAmount);
+
+// Task-05
+// let kmhSpeed = 36;
+// let msSpeed = kmhSpeed*1000/3600;
+// console.log(msSpeed);
+
+// Task-06
+// let age = prompt("Nece yasiniz var?");
+// let days = age*365;
+// console.log(days);
+
+// Task-07
+let num = 37;
+console.log(num**5);
+console.log(num%3);
+
+
+
+
+
+
